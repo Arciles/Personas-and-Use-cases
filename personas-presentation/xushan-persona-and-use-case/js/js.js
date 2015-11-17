b3b73b4f13bd20a217268cc9f68b2f9b5e1960e5
Theodore Foss <fritzfoss@hotmail.com>
2015-11-16 23:01:57 -0500

    Group 7 Presentation Web Pages
    Here is the main site with links to the individual pages for each of
    our personas/use cases. Each page is a mini site in its own folder

---
+++ personas-presentation/xushan-persona-and-use-case/js/js.js
@@ -0,0 +1,206 @@
+	// -webkit-box-shadow: 0 5px 12px rgba(0, 0, 0, 0.3);
+	// -moz-box-shadow: 0 5px 12px rgba(0, 0, 0, 0.3);
+	// box-shadow: 0 5px 12px rgba(0, 0, 0, 0.3);
+
+
+$(document).ready(function(){
+	
+	// line 1
+	$("li#one").mouseover(function(){
+	$("li#one").css("font-size", "1.4em");
+	$("li#one").css("margin-top", "-10px");
+	$("li#one").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#one").mouseout(function(){
+	$("li#one").css("-webkit-box-shadow", "none");
+	$("li#one").css("font-size", "16px");
+	$("li#one").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 2
+	$("li#two").mouseover(function(){
+	$("li#two").css("font-size", "1.4em");
+	$("li#two").css("margin-top", "-10px");
+	$("li#two").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#two").mouseout(function(){
+	$("li#two").css("-webkit-box-shadow", "none");
+	$("li#two").css("font-size", "16px");
+	$("li#two").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 3
+	$("li#three").mouseover(function(){
+	$("li#three").css("font-size", "1.4em");
+	$("li#three").css("margin-top", "-10px");
+	$("li#three").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#three").mouseout(function(){
+	$("li#three").css("-webkit-box-shadow", "none");
+	$("li#three").css("font-size", "16px");
+	$("li#three").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 4
+	$("li#four").mouseover(function(){
+	$("li#four").css("font-size", "1.4em");
+	$("li#four").css("margin-top", "-10px");
+	$("li#four").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#four").mouseout(function(){
+	$("li#four").css("-webkit-box-shadow", "none");
+	$("li#four").css("font-size", "16px");
+	$("li#four").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 5
+	$("li#five").mouseover(function(){
+	$("li#five").css("font-size", "1.4em");
+	$("li#five").css("margin-top", "-10px");
+	$("li#five").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#five").mouseout(function(){
+	$("li#five").css("-webkit-box-shadow", "none");
+	$("li#five").css("font-size", "16px");
+	$("li#five").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 6
+	$("li#six").mouseover(function(){
+	$("li#six").css("font-size", "1.4em");
+	$("li#six").css("margin-top", "-10px");
+	$("li#six").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#six").mouseout(function(){
+	$("li#six").css("-webkit-box-shadow", "none");
+	$("li#six").css("font-size", "16px");
+	$("li#six").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 7
+	$("li#seven").mouseover(function(){
+	$("li#seven").css("font-size", "1.4em");
+	$("li#seven").css("margin-top", "-10px");
+	$("li#seven").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#seven").mouseout(function(){
+	$("li#seven").css("-webkit-box-shadow", "none");
+	$("li#seven").css("font-size", "16px");
+	$("li#seven").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 8
+	$("li#eight").mouseover(function(){
+	$("li#eight").css("font-size", "1.4em");
+	$("li#eight").css("margin-top", "-10px");
+	$("li#eight").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#eight").mouseout(function(){
+	$("li#eight").css("-webkit-box-shadow", "none");
+	$("li#eight").css("font-size", "16px");
+	$("li#eight").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 9
+	$("li#nine").mouseover(function(){
+	$("li#nine").css("font-size", "1.4em");
+	$("li#nine").css("margin-top", "-10px");
+	$("li#nine").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#nine").mouseout(function(){
+	$("li#nine").css("-webkit-box-shadow", "none");
+	$("li#nine").css("font-size", "16px");
+	$("li#nine").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 10
+	$("li#ten").mouseover(function(){
+	$("li#ten").css("font-size", "1.4em");
+	$("li#ten").css("margin-top", "-10px");
+	$("li#ten").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#ten").mouseout(function(){
+	$("li#ten").css("-webkit-box-shadow", "none");
+	$("li#ten").css("font-size", "16px");
+	$("li#ten").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 11
+	$("li#eleven").mouseover(function(){
+	$("li#eleven").css("font-size", "1.4em");
+	$("li#eleven").css("margin-top", "-10px");
+	$("li#eleven").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#eleven").mouseout(function(){
+	$("li#eleven").css("-webkit-box-shadow", "none");
+	$("li#eleven").css("font-size", "16px");
+	$("li#eleven").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 12
+	$("li#twelve").mouseover(function(){
+	$("li#twelve").css("font-size", "1.4em");
+	$("li#twelve").css("margin-top", "-10px");
+	$("li#twelve").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#twelve").mouseout(function(){
+	$("li#twelve").css("-webkit-box-shadow", "none");
+	$("li#twelve").css("font-size", "16px");
+	$("li#twelve").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 13 
+	$("li#thirteen").mouseover(function(){
+	$("li#thirteen").css("font-size", "1.4em");
+	$("li#thirteen").css("margin-top", "-10px");
+	$("li#thirteen").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#thirteen").mouseout(function(){
+	$("li#thirteen").css("-webkit-box-shadow", "none");
+	$("li#thirteen").css("font-size", "16px");
+	$("li#thirteen").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 14 (User Cases section)
+	$("li#fourteen").mouseover(function(){
+	$("li#fourteen").css("font-size", "1.4em");
+	$("li#fourteen").css("margin-top", "-9px");
+	$("li#fourteen").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#fourteen").mouseout(function(){
+	$("li#fourteen").css("-webkit-box-shadow", "none");
+	$("li#fourteen").css("font-size", "16px");
+	$("li#fourteen").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 15 (User Cases section)
+	$("li#fifteen").mouseover(function(){
+	$("li#fifteen").css("font-size", "1.4em");
+	$("li#fifteen").css("margin-top", "-9px");
+	$("li#fifteen").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#fifteen").mouseout(function(){
+	$("li#fifteen").css("-webkit-box-shadow", "none");
+	$("li#fifteen").css("font-size", "16px");
+	$("li#fifteen").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	// line 16 (User Cases section)
+	$("li#sixteen").mouseover(function(){
+	$("li#sixteen").css("font-size", "1.4em");
+	$("li#sixteen").css("margin-top", "-9px");
+	$("li#sixteen").css("-webkit-box-shadow", "0 5px 12px rgba(0, 0, 0, 0.3)");
+	}); // End Mouseover 
+	$("li#sixteen").mouseout(function(){
+	$("li#sixteen").css("-webkit-box-shadow", "none");
+	$("li#sixteen").css("font-size", "16px");
+	$("li#sixteen").css("margin-top", "6px");
+	}); // End Mouseout
+	
+	
+	
+	
+	
+	
+	
+});
